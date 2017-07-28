94ba35e9398a3d2d6a088e741972eab089a5f8cf
mac <mac@macs-iMac-2.local>
2017-07-28 20:03:06 +0530

    QRcode scanner complete

--- QRCodeScanner/ViewController.swift
+++ QRCodeScanner/ViewController.swift
@@ -7,12 +7,54 @@
 //
 
 import UIKit
+import AVFoundation  //Audio Video Foundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    @IBOutlet weak var img_square: UIImageView!
+    
+    var video = AVCaptureVideoPreviewLayer()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //creating session
+        let session = AVCaptureSession()
+        
+        //Define capture Device
+        
+        
+        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        let input: AVCaptureDeviceInput
+        
+        do{
+            input = try AVCaptureDeviceInput(device: captureDevice)
+            session.addInput(input)
+        }catch{
+            return
+        }
+        
+        if (session.canAddInput(input)){
+            session.addInput(input)
+        }else{
+            failed()
+            return
+        }
+        
+        let output = AVCaptureMetadataOutput()
+        session.addOutput(output)
+        
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        output.metadataObjectTypes = [AVMetadataObjectTypeQRCode] // you can assign any thing like face AVMetadataObjectTypeFace
+        
+        video = AVCaptureVideoPreviewLayer(session: session)
+        video.frame = view.layer.bounds
+        view.layer.addSublayer(video)
+        
+        self.view.bringSubview(toFront: img_square)
+        
+        session.startRunning()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +62,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: Creat function for capturing output process while scaning QRCode
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        if metadataObjects != nil && metadataObjects.count != 0{
+            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject{
+                if object.type == AVMetadataObjectTypeQRCode{
+                    let alert = UIAlertController(title: "QR Code", message: object.stringValue, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Retake", style: .default, handler: nil))
+                    alert.addAction(UIAlertAction(title: "Copy", style: .default, handler: { (nil) in
+                        UIPasteboard.general.string = object.stringValue
+                    }))
+                    present(alert, animated: true, completion: nil)
+                }
+            }
+        }
+    }
 
+    
+    func failed() {
+        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
 }
 
